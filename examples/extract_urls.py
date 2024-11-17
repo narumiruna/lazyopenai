79b78fe91675974564ac4a99c35a7889c52adb8a
narumi <toucans-cutouts0f@icloud.com>
2024-11-17 23:19:05 +0800

    rename

---
+++ examples/extract_urls.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from pydantic import Field
+
+from lazyopenai import generate
+
+
+class URL(BaseModel):
+    url: str = Field(..., description="The URL")
+    contain_video: bool = Field(..., description="Whether the URL contains a video")
+
+
+class URLList(BaseModel):
+    urls: list[URL] = Field(..., description="The list of URLs")
+
+
+def main() -> None:
+    prompt = """
+    https://www.google.com.tw
+    https://www.youtube.com/watch?v=9bZkp7q19f0
+    """.strip()
+
+    resp = generate(prompt, response_format=URLList)
+    print(resp)
+
+
+if __name__ == "__main__":
+    main()
