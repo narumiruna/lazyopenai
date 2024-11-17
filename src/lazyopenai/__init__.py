5e910013665e93a11da696e64dbcd6ebb852e77c
narumi <toucans-cutouts0f@icloud.com>
2024-11-17 16:08:08 +0800

    move create_embeddings

--- src/lazyopenai/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chat import async_create
-from .chat import async_parse
-from .chat import create
-from .chat import create_embeddings
-from .chat import parse
--- src/lazyopenai/chat.py
+++ src/lazyopenai/chat.py
@@ -146,25 +146,3 @@ async def async_parse(messages: Iterable[ChatCompletionMessageParam], response_f
 
     return parsed
 
-
-def create_embeddings(texts: str | list[str]) -> CreateEmbeddingResponse:
-    """
-    Creates embeddings for the given text or list of texts.
-
-    Args:
-        texts (str | list[str]): The text or list of texts to create embeddings for.
-
-    Returns:
-        CreateEmbeddingResponse: The response containing the created embeddings.
-
-    Raises:
-        ValueError: If the input texts are not a string or list of strings.
-    """
-    if isinstance(texts, str):
-        texts = [texts]
-
-    client = get_client()
-    model = get_embedding_model()
-
-    response = client.embeddings.create(input=texts, model=model)
-    return response
