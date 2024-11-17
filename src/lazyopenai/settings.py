d81a52da0a44f8c002adea5bd7da06c0182dde62
narumi <toucans-cutouts0f@icloud.com>
2024-11-17 18:25:57 +0800

    fix type error

--- src/lazyopenai/settings.py
+++ src/lazyopenai/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    api_key: str = Field(default=None, description="The OpenAI API key.")
+    api_key: str | None = Field(default=None, description="The OpenAI API key.")
     model: str = Field(default="gpt-4o-mini", description="The OpenAI model name.")
     temperature: float = Field(default=0.0, description="The OpenAI temperature setting.")
     embedding_model: str = Field(default="text-embedding-3-small", description="The OpenAI embedding model.")
