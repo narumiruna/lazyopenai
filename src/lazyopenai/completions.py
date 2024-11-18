76e7e5482816faf2d1f5b685f683f119d0941628
narumi <toucans-cutouts0f@icloud.com>
2024-11-18 13:01:29 +0800

    delete async function and refactor

--- src/lazyopenai/completions.py
+++ src/lazyopenai/completions.py
@@ -2,25 +2,18 @@ import json
 from typing import TypeVar
 
 import openai
+from openai import OpenAI
 from pydantic import BaseModel
 
 from .settings import settings
-from .utils import get_async_client
-from .utils import get_client
 
 T = TypeVar("T", bound=BaseModel)
-S = TypeVar("S", bound=BaseModel)
 
 
-def get_tool(tools: list[type[S]], name: str) -> type[S]:
-    for tool in tools:
-        if tool.__name__ == name:
-            return tool
-    raise ValueError(f"Tool {name} not found")
+def create(messages, tools: list[type[BaseModel]] | None = None) -> str:
+    client = OpenAI(api_key=settings.api_key)
 
-
-def create(messages, tools: list[type[S]] | None = None) -> str:
-    client = get_client()
+    tool_map = {tool.__name__: tool for tool in tools} if tools else {}
 
     kwargs = {}
     if tools:
@@ -39,14 +32,14 @@ def create(messages, tools: list[type[S]] | None = None) -> str:
         if choice.finish_reason == "tool_calls" and choice.message.tool_calls:
             tool_messages = []
             for tool_call in choice.message.tool_calls:
-                tool_function = get_tool(tools, tool_call.function.name)
-                if not tool_function:
+                tool = tool_map.get(tool_call.function.name)
+                if not tool:
                     continue
-                tool_arguments = json.loads(tool_call.function.arguments)
+                args = json.loads(tool_call.function.arguments)
                 tool_messages.append(
                     {
                         "role": "tool",
-                        "content": str(tool_function(**tool_arguments)()),  # type: ignore
+                        "content": str(tool(**args)()),  # type: ignore
                         "tool_call_id": tool_call.id,
                     }
                 )
@@ -66,27 +59,10 @@ def create(messages, tools: list[type[S]] | None = None) -> str:
     return content
 
 
-async def async_create(messages) -> str:
-    client = get_async_client()
+def parse(messages, response_format: type[T], tools: list[type[BaseModel]] | None = None) -> T:
+    client = OpenAI(api_key=settings.api_key)
 
-    response = await client.chat.completions.create(
-        messages=messages,
-        model=settings.model,
-        temperature=settings.temperature,
-    )
-
-    if not response.choices:
-        raise ValueError("No completion choices returned")
-
-    content = response.choices[0].message.content
-    if not content:
-        raise ValueError("No completion message content")
-
-    return content
-
-
-def parse(messages, response_format: type[T], tools: list[type[S]] | None = None) -> T:
-    client = get_client()
+    tool_map = {tool.__name__: tool for tool in tools} if tools else {}
 
     kwargs = {}
     if tools:
@@ -106,14 +82,14 @@ def parse(messages, response_format: type[T], tools: list[type[S]] | None = None
         if choice.finish_reason == "tool_calls" and choice.message.tool_calls:
             tool_messages = []
             for tool_call in choice.message.tool_calls:
-                tool_function = get_tool(tools, tool_call.function.name)
-                if not tool_function:
+                tool = tool_map.get(tool_call.function.name)
+                if not tool:
                     continue
-                tool_arguments = json.loads(tool_call.function.arguments)
+                args = json.loads(tool_call.function.arguments)
                 tool_messages.append(
                     {
                         "role": "tool",
-                        "content": str(tool_function(**tool_arguments)()),  # type: ignore
+                        "content": str(tool(**args)()),  # type: ignore
                         "tool_call_id": tool_call.id,
                     }
                 )
@@ -123,26 +99,6 @@ def parse(messages, response_format: type[T], tools: list[type[S]] | None = None
                 temperature=settings.temperature,
                 response_format=response_format,
             )
-
-    if not response.choices:
-        raise ValueError("No completion choices returned")
-
-    parsed = response.choices[0].message.parsed
-    if not parsed:
-        raise ValueError("No completion message parsed")
-
-    return parsed
-
-
-async def async_parse(messages, response_format: type[T]) -> T:
-    client = get_async_client()
-
-    response = await client.beta.chat.completions.parse(
-        messages=messages,
-        model=settings.model,
-        temperature=settings.temperature,
-        response_format=response_format,
-    )
 
     if not response.choices:
         raise ValueError("No completion choices returned")
