0059bfffff8b6a2149760f68949fd4177dae91ba
narumi <toucans-cutouts0f@icloud.com>
2024-12-04 01:19:23 +0800

    set default loguru level to INFO

--- src/lazyopenai/__init__.py
+++ src/lazyopenai/__init__.py
@@ -7,5 +7,5 @@ from loguru import logger
 from .lazy import create_chat
 from .lazy import generate
 
-LOGURU_LEVEL: Final[str] = os.getenv("LOGURU_LEVEL", "INFO").upper()
+LOGURU_LEVEL: Final[str] = os.getenv("LOGURU_LEVEL", "INFO")
 logger.configure(handlers=[{"sink": sys.stderr, "level": LOGURU_LEVEL}])
